64f8c49830b314b9f5baf42fa22638d7d6e6ae24
jkweg <jakub.wegrzyniak1239@gmail.com>
2026-04-17 10:55:31 +0200

    Lab4 poprawka

--- Lab4/zad4/zadanie4.jl
+++ Lab4/zad4/zadanie4.jl
@@ -3,6 +3,21 @@ using Polynomials
 using Statistics
 
 
+function lagrange_interpolate(x_nodes, y_nodes, x_val)
+    n = length(x_nodes)
+    wynik = 0.0
+    for i in 1:n
+        L_i = 1.0
+        for j in 1:n
+            if i != j
+                L_i *= (x_val - x_nodes[j]) / (x_nodes[i] - x_nodes[j])
+            end
+        end
+        wynik += y_nodes[i] * L_i
+    end
+    return wynik
+end
+
 function newton_coef(x, y)
     n = length(x)
     c = copy(y)
@@ -20,21 +35,23 @@ function newton_eval(x, c, x_val)
     return val
 end
 
+lagrange_interpolate([1.0, 2.0], [1.0, 2.0], 1.5)
 newton_eval([1.0, 2.0], newton_coef([1.0, 2.0], [1.0, 2.0]), 1.5)
 fit([1.0, 2.0], [1.0, 2.0])(1.5)
 
+rozmiary = [10, 50, 100, 200, 300, 500]
+liczba_prob = 10
+x_test = 0.5
 
-rozmiary = [10, 50, 100, 200, 300, 500] 
-liczba_prob = 10                        
-x_test = 0.5                            
+sr_bud_n = Float64[]; odch_bud_n = Float64[]
+sr_ewal_n = Float64[]; odch_ewal_n = Float64[]
 
-sr_budowa_n = Float64[]; odch_budowa_n = Float64[]
-sr_ewal_n = Float64[];   odch_ewal_n = Float64[]
+sr_bud_p = Float64[]; odch_bud_p = Float64[]
+sr_ewal_p = Float64[]; odch_ewal_p = Float64[]
 
-sr_budowa_p = Float64[]; odch_budowa_p = Float64[]
-sr_ewal_p = Float64[];   odch_ewal_p = Float64[]
+sr_ewal_l = Float64[]; odch_ewal_l = Float64[] 
 
-println("Rozpoczynam pomiary... to może chwilę potrwać.")
+println("Rozpoczynam pomiary z dodanym Lagrange'm...")
 
 for n in rozmiary
     x_nodes = collect(1.0:n)
@@ -42,6 +59,7 @@ for n in rozmiary
     
     czasy_bud_n = zeros(liczba_prob); czasy_ewal_n = zeros(liczba_prob)
     czasy_bud_p = zeros(liczba_prob); czasy_ewal_p = zeros(liczba_prob)
+    czasy_ewal_l = zeros(liczba_prob)
     
     for i in 1:liczba_prob
         czasy_bud_n[i] = @elapsed c = newton_coef(x_nodes, y_nodes)
@@ -49,24 +67,29 @@ for n in rozmiary
         
         czasy_bud_p[i] = @elapsed wielomian = fit(x_nodes, y_nodes)
         czasy_ewal_p[i] = @elapsed wielomian(x_test)
+        
+        czasy_ewal_l[i] = @elapsed lagrange_interpolate(x_nodes, y_nodes, x_test)
     end
     
-    push!(sr_budowa_n, mean(czasy_bud_n)); push!(odch_budowa_n, std(czasy_bud_n))
-    push!(sr_ewal_n, mean(czasy_ewal_n));  push!(odch_ewal_n, std(czasy_ewal_n))
+    push!(sr_bud_n, mean(czasy_bud_n)); push!(odch_bud_n, std(czasy_bud_n))
+    push!(sr_ewal_n, mean(czasy_ewal_n)); push!(odch_ewal_n, std(czasy_ewal_n))
     
-    push!(sr_budowa_p, mean(czasy_bud_p)); push!(odch_budowa_p, std(czasy_bud_p))
-    push!(sr_ewal_p, mean(czasy_ewal_p));  push!(odch_ewal_p, std(czasy_ewal_p))
+    push!(sr_bud_p, mean(czasy_bud_p)); push!(odch_bud_p, std(czasy_bud_p))
+    push!(sr_ewal_p, mean(czasy_ewal_p)); push!(odch_ewal_p, std(czasy_ewal_p))
+    
+    push!(sr_ewal_l, mean(czasy_ewal_l)); push!(odch_ewal_l, std(czasy_ewal_l))
 end
 
-p1 = plot(rozmiary, sr_budowa_n, yerr=odch_budowa_n, label="Newton", marker=:circle, 
-          title="Czas TWORZENIA wielomianu", xlabel="Liczba węzłów (n)", ylabel="Czas [s]", color=:green)
-plot!(p1, rozmiary, sr_budowa_p, yerr=odch_budowa_p, label="Polynomials", marker=:square, color=:blue)
+
+p1 = plot(rozmiary, sr_bud_n, yerr=odch_bud_n, label="Newton", marker=:circle, 
+          title="Czas TWORZENIA", xlabel="Liczba węzłów", ylabel="Czas [s]", color=:green)
+plot!(p1, rozmiary, sr_bud_p, yerr=odch_bud_p, label="Polynomials", marker=:square, color=:blue)
 
 p2 = plot(rozmiary, sr_ewal_n, yerr=odch_ewal_n, label="Newton", marker=:circle, 
-          title="Czas EWALUACJI w punkcie", xlabel="Liczba węzłów (n)", ylabel="Czas [s]", color=:green)
+          title="Czas EWALUACJI", xlabel="Liczba węzłów", ylabel="Czas [s]", color=:green)
 plot!(p2, rozmiary, sr_ewal_p, yerr=odch_ewal_p, label="Polynomials", marker=:square, color=:blue)
+plot!(p2, rozmiary, sr_ewal_l, yerr=odch_ewal_l, label="Lagrange", marker=:utriangle, color=:red)
 
 wykres = plot(p1, p2, layout=(1,2), size=(1000, 500), margin=5Plots.mm)
 display(wykres)
-
-savefig("zadanie4_pomiary_czasu.png")
+savefig("zadanie4_pomiary_czasu_z_lagrangem.png")
